15b0899a99f3a1d4732ce198b82a04e03584123c
Юрий Белюшин <yuriy.belyushin@instamart.ru>
2023-03-28 22:17:40 +0500

    Initial commit

---
+++ ParsePemToXML/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.VisualBasic;
+
+TextReader reader;
+if (args.Length > 0) {
+    reader = new StreamReader(new FileStream(args[0], FileMode.Open));
+}
+else
+{
+    reader = Console.In;
+    
+}
+var key = reader.ReadToEnd();
+var encryptor = RSA.Create();
+encryptor.ImportFromPem(key.ToCharArray());
+var isPrivate = Strings.InStr(key, "PRIVATE") != 0;
+
+Console.WriteLine(encryptor.ToXmlString(isPrivate));
